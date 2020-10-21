a91dcd26a2ea568a11089bd751c03562f781d918
Maksym <maksym.kozachuk@protonmail.com>
2020-10-21 15:00:51 +0200

    findByCode, findByRenderer, count implementation

--- src/main/java/MyStructure.java
+++ src/main/java/MyStructure.java
@@ -1,13 +1,71 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class MyStructure implements IMyStructure {
+    private List<INode> nodes = new LinkedList<>();
+
+    private List<INode> compositeNodes = new LinkedList<>();
+
     public INode findByCode(String code) {
-        return null;
+        return search(code);
     }
 
     public INode findByRenderer(String renderer) {
-        return null;
+        return search(renderer);
     }
 
     public int count() {
-        return 0;
+        int counter = 0;
+
+        for (INode node : nodes) {
+            if (isICompositeNode(node)) {
+                counter++;
+                counter += findAllInnerNodes((ICompositeNode) node).size();
+            } else {
+                counter++;
+            }
+        }
+
+        return counter;
     }
+
+    private List<INode> findAllInnerNodes(ICompositeNode node) {
+        compositeNodes.addAll(node.getNodes());
+        for (INode node1 : node.getNodes()) {
+            if (isICompositeNode(node1)) {
+                compositeNodes.addAll(((ICompositeNode) node1).getNodes());
+            }
+        }
+
+        return compositeNodes;
+    }
+
+    private boolean isICompositeNode(INode node) {
+        return node instanceof ICompositeNode;
+    }
+
+    private INode search(String codeOrRenderer) {
+        if (codeOrRenderer == null) {
+            throw new IllegalArgumentException();
+        }
+        for (INode node : nodes) {
+
+            if (isICompositeNode(node)) {
+                findAllInnerNodes((ICompositeNode) node);
+            }
+        }
+
+        compositeNodes.addAll(nodes);
+        for (INode node : compositeNodes) {
+            if (node.getCode().equals(codeOrRenderer)) {
+                return node;
+            }
+            if (node.getRenderer().equals(codeOrRenderer)) {
+                return node;
+            }
+        }
+
+        return null;
+    }
+
 }
